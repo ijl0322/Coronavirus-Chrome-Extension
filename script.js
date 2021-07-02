92eba4d9db7340d6a0b2ee054b2a2ca565f56b86
Isabel Lee <ijl0322@users.noreply.github.com>
2021-07-03 00:13:26 +0800

    block dcard

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-replaceText(document.body)
-
-function replaceText(element) {
-  if (element.hasChildNodes()) {
-    element.childNodes.forEach(replaceText)
-  } else if (element.nodeType === Text.TEXT_NODE) {
-    if (element.textContent.match(/coronavirus/gi)) {
-      const newElement = document.createElement('span')
-      newElement.innerHTML = element.textContent.replace(/(coronavirus)/gi, '<span class="rainbow">$1</span>')
-      element.replaceWith(newElement)
-    }
-  }
-}
+//var content = 'Don\'t read this!';
+//document.getElementsByTagName('body')[0].innerHTML = content; 
+window.location.replace("https://www.fool.com");
